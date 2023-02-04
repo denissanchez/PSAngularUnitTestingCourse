febe3b7fbe34cdc9796c78c5b5d084add6cceae4
Denis Sanchez <desanchezca@gmail.com>
2023-02-04 12:11:00 -0500

    Add tests for HeroComponent

---
+++ src/app/hero/hero.component.spec.ts
@@ -0,0 +1,42 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HeroComponent } from "./hero.component";
+import { By } from "@angular/platform-browser";
+
+describe("HeroComponent (shallow tests)", () => {
+  let fixture: ComponentFixture<HeroComponent>;
+  let component: HeroComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+
+    fixture = TestBed.createComponent(HeroComponent);
+    component = fixture.componentInstance;
+  });
+
+  it("should have the correct hero", () => {
+    fixture.componentInstance.hero = {
+      id: 1,
+      name: "Super Man",
+      strength: 3,
+    };
+
+    expect(fixture.componentInstance.hero.name).toEqual("Super Man");
+  });
+
+  it("should render the hero name in an anchor tag", () => {
+    fixture.componentInstance.hero = {
+      id: 1,
+      name: "Iron Man",
+      strength: 15,
+    };
+    
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('a')).nativeElement.textContent).toContain('Iron Man')
+    expect(fixture.nativeElement.querySelector('a').textContent).toContain('Iron Man')
+  });
+});
